a316824c0a8a00a038ebae4fe77fab340893e1af
veen-git <sakhamuri.praveen@gmail.com>
2024-08-21 18:14:08 -0700

    Add computer choice function
    create a function to return "rock", "paper", or 'scissors" when called using Math.random() function

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("test")
+
+function getComputerChoice() {
+    let i = Math.random();
+        if (i <= 0.33) {
+          return "rock";
+         }
+        
+        else if (0.33 < i <= 0.66) {
+           return "paper";
+        }
+
+        else {
+            return "scissors";
+        }    
+    
+}
+
