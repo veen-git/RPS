4e28c0c44528538539ff8659906012c6692c9579
veen-git <sakhamuri.praveen@gmail.com>
2024-08-22 01:09:07 -0700

    Add playGame function
    Create playGame function. Add playRound and score variables into function. Write conditional statements to declare winnder after 5 rounds of playRound.

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-
-
-let humanScore = 0
-let computerScore = 0
-
 function getComputerChoice() {
     let i = Math.random();
         if (i <= 0.33) {
@@ -26,7 +19,7 @@ function getHumanChoice() {
 
     let choice = prompt("Pick one: rock, paper, or scissors");
         if (choice === null) {
-        return getHumanChoice();
+        return;
     }    
     
         else if (choice.toLowerCase() == "rock" || choice.toLowerCase() == "paper" || choice.toLowerCase() == "scissors") {
@@ -39,59 +32,112 @@ function getHumanChoice() {
             
     }
 
-function playRound(humanSelection,computerSelection) {
 
-    if (humanSelection == "rock") {
-        if (computerSelection == "scissors") {
-            humanScore += 1;
-            return ("Congrats, You win!!");   
+function playGame() {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanSelection,computerSelection) {
+
+        if (humanSelection == "rock") {
+
+            if (computerSelection == "scissors") {
+                humanScore += 1;
+                console.log("your choice: rock [] computer choice: scissors")
+                console.log ("Congrats, You win!!");   
+            }
+            else if (computerSelection == "rock") {
+                console.log("your choice: rock [] computer choice: rock")
+                console.log("It's a draw.");
+            }
+            else {
+                computerScore += 1;
+                console.log("your choice: rock [] computer choice: paper")
+                console.log("Computer wins, bad luck.");
+            }   
         }
 
-        else if (computerSelection == "rock") {
-            return "It's a draw.";
+
+        else if (humanSelection == "scissors") {
+
+            if (computerSelection == "paper") {
+                humanScore += 1;
+                console.log("your choice: scissors [] computer choice: paper")
+                console.log("Congrats, You win!!"); 
+            }
+            else if (computerSelection == "scissors") {
+                console.log("your choice: scissors [] computer choice: scissors")
+                console.log("It's a draw.");
+            }
+            else {
+                computerScore += 1;
+                console.log("your choice: scissors [] computer choice: rock")
+                console.log("Computer wins, bad luck.");
+            }
         }
+
+
         else {
-            computerScore += 1;
-            return "Computer wins, bad luck.";
-        }    
+            if (computerSelection == "rock") {
+                humanScore += 1;
+                console.log("your choice: paper [] computer choice: rock")
+                console.log("Congrats, You win!!");
+            }
+            else if (computerSelection == "paper") {
+                console.log("your choice: paper [] computer choice: paper")
+                console.log("It's a draw.");
+            }
+            else {
+                computerScore += 1;
+                console.log("your choice: paper [] computer choice: scissors")
+                console.log("Computer wins, bad luck.");
+            }
+        }
+
+
+        console.log("your Score: " + humanScore + "  Computer Score: " + computerScore)
+
     }
 
-    else if (humanSelection == "scissors") {
-        if (computerSelection == "paper") {
-            humanScore += 1;
-            return "Congrats, You win!!"; 
-        }
-        else if (computerSelection == "scissors") {
-            return "It's a draw."
-        }
-        else {
-            computerScore += 1;
-            return "Computer wins, bad luck.";
-        }
-    }
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+playRound(humanChoice,computerChoice);
 
-    else {
-        if (computerSelection == "rock") {
-            humanScore += 1;
-            return "Congrats, You win!!";
-        }
-        else if (computerSelection == "paper") {
-            return "It's a draw."
-        }
-        else {
-            computerScore += 1;
-            return "Computer wins, bad luck.";
-        }
-    }
- 
+humanChoice = getHumanChoice();
+computerChoice = getComputerChoice();
+playRound(humanChoice,computerChoice);    
+
+humanChoice = getHumanChoice();
+computerChoice = getComputerChoice();
+playRound(humanChoice,computerChoice);    
+
+humanChoice = getHumanChoice();
+computerChoice = getComputerChoice();
+playRound(humanChoice,computerChoice);    
+
+humanChoice = getHumanChoice();
+computerChoice = getComputerChoice();
+playRound(humanChoice,computerChoice);    
+
+console.log(".")
+console.log(".")
+console.log(".")
+
+if (computerScore > humanScore) {
+    console.log("Game over! Computer wins :(")
 }
 
-console.log(playRound(humanChoice,computerChoice));
+else if (humanScore > computerScore) {
+    console.log("Good Job, You win!!!")
+}
 
-let Statement = "You picked " + humanChoice + " and Computer picked " + computerChoice;
-let scoreStatement = "Your score: " + humanScore + "  " + "Computer score: " + computerScore;
+else {
+    console.log("It's a tie, nice try!")
+}
+}
+        
 
-console.log(Statement);
-console.log(scoreStatement);
+console.log(playGame())
 
-
+  
